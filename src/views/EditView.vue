fb91478e7b52c527ee71950f9af78440e5dcfaea
Por Pattarapong <porz.porz.porz@gmail.com>
2024-03-04 14:19:28 +0700

    use CSS daisyUI

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -2,11 +2,13 @@
 import { useTodoStore } from '../stores/todo'
 import { RouterLink, useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
+import Loading from '../components/Loading.vue';
 
 const todoStore = useTodoStore()
 const route = useRoute()
 const todoId = ref(-1)
 const isLoaded = ref(false)
+const isUpdated = ref(false)
 
 onMounted(async () => {
     await todoStore.loadTodo(route.params.id)
@@ -15,14 +17,19 @@ onMounted(async () => {
 
 })
 
-const editTodo = async(selectedTodo) =>{
+const editTodo = async (selectedTodo) => {
     try {
+        isLoaded.value = false
         const bodyData = {
-        name: selectedTodo.name,
-        status: selectedTodo.status
-    }
-    await todoStore.editTodo(bodyData, todoId.value)
-    alert('edit todo complete')
+            name: selectedTodo.name,
+            status: selectedTodo.status
+        }
+        await todoStore.editTodo(bodyData, todoId.value)
+        isLoaded.value = true
+        isUpdated.value = true
+        setTimeout(()=> {
+            isUpdated.value = false
+        },2500)
     } catch (error) {
         console.log('error', error)
     }
@@ -31,36 +38,49 @@ const editTodo = async(selectedTodo) =>{
 </script>
 
 <template>
-    <div v-if="isLoaded">
-        Edit ID: {{ todoId }}
-        <div class="flex">
-            <div>
-               Name
-            </div> 
-            <input type="text" v-model="todoStore.selectedTodo.name">
-        </div>
-        <div class="flex">
-            <div>
-                Status 
+    <div class="w-1/2 mx-auto">
+        <div class="toast toast-end">
+            <div v-if="isUpdated" class="alert alert-success">
+                <span>Update Successfully.</span>
             </div>
-            <select v-model="todoStore.selectedTodo.status" >
-                <option value="">Select Status</option>
-                <option v-for="status in todoStore.statuses" :value="status">
-                    {{ status }}
-                </option>
-            </select>
         </div>
-        <div>
-            <button @click="editTodo(todoStore.selectedTodo)">Edit</button>
-            <RouterLink :to="{name:'todo-list'}">
-                <button>Back</button>
-            </RouterLink>
+        <div v-if="isLoaded">
+            EDIT ID :
+            <div class="badge badge-outline"> {{ todoId }}</div>
+            <div>
+                <label class="form-control w-full ">
+                    <div class="label">
+                        <span class="label-text">Name</span>
+                    </div>
+                    <input type="text" placeholder="Type name todo" class="input input-bordered w-full"
+                        v-model="todoStore.selectedTodo.name" />
+                </label>
+            </div>
+
+            <div>
+                <label class="form-control w-full ">
+                    <div class="label">
+                        <span class="label-text">Status</span>
+                    </div>
+
+                    <select class="select select-bordered w-full" v-model="todoStore.selectedTodo.status">
+                        <option value="">Select Status</option>
+                        <option v-for="status in todoStore.statuses" :value="status">
+                            {{ status }}
+                        </option>>
+                    </select>
+                </label>
+            </div>
+            <div>
+                <button class="btn btn-primary w-full mt-5" @click="editTodo(todoStore.selectedTodo)">Edit</button>
+                <RouterLink :to="{ name: 'todo-list' }">
+                    <button class="btn btn-outline  w-full mt-1 ">Back</button>
+                </RouterLink>
+            </div>
         </div>
-    </div>
-    <div v-else>
-        <h2>
-            is Loading...
-        </h2>
+        <div v-else>
+            <Loading></Loading>
+        </div>
     </div>
 
-</template> 
+</template>
